99ac41052383dd6ab19c4934a6ca650be5e0f4e6
Alexander Popov <alex.popov.tech@gmail.com>
2019-02-25 21:53:14 +0200

    update config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -25,15 +25,15 @@ const siteConfig = {
 
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://aleksanderpopov.github.io', // Your website URL
-  baseUrl: '/proximax-storage/', // Base URL for your project */
+  url: 'https://proximax-storage.github.io', // Your website URL
+  baseUrl: '/proximax-storage-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'proximax-storage',
-  organizationName: 'aleksanderpopov',
+  projectName: 'proximax-storage-docs',
+  organizationName: 'proximax-storage',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
