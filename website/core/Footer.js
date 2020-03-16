123ae076ca872323e0857f6189119dd1601b8091
Yevhen <iorhachov.yevhen@482.solutions>
2020-03-16 15:15:45 +0200

    update site navigation

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -40,32 +40,71 @@ class Footer extends React.Component {
             <a href={this.docUrl('roles/Owner', this.props.docsPart)}>
               Owner
             </a>
-            <a href={this.docUrl('roles/replicator', this.props.docsPart)}>
+            <a href={this.docUrl('roles/replicator#replicator', this.props.docsPart)}>
               Replicator
             </a>
-            <a href={this.docUrl('roles/verifier', this.props.docsPart)}>
+            <a href={this.docUrl('roles/replicator#verifier', this.props.docsPart)}>
               Verifier
+            </a>
+            <a href={this.docUrl('roles/executor', this.props.docsPart)}>
+              Executor
             </a>
           </div>
           <div>
             <h5>Built-in Features</h5>
-            <a href={this.docUrl('built_in_features/exchange', this.props.docsPart)}>
-              Exchange
-            </a>
             <a href={this.docUrl('built_in_features/drive/overview', this.props.docsPart)}>
               Drive
+            </a>
+            <a href={this.docUrl('built_in_features/Supercontract/overview', this.props.docsPart)}>
+              SuperContract
             </a>
             <a href={this.docUrl('built_in_features/challenge', this.props.docsPart)}>
               Challenge
             </a>
+            <a href={this.docUrl('built_in_features/rewards', this.props.docsPart)}>
+              Rewards
+            </a>
           </div>
           <div>
-            <h5>Protocol</h5>
+            <h5>Protocols</h5>
             <a href={this.docUrl('protocols/cross_block_protocol', this.props.docsPart)}>
               Cross-block protocol
             </a>
             <a href={this.docUrl('protocols/fair_streaming', this.props.docsPart)}>
               Fair streaming
+            </a>
+          </div>
+          <div>
+            <h5>Algorithms</h5>
+            <a href={this.docUrl('algorithms/verification', this.props.docsPart)}>
+              Verification
+            </a>
+            <a href={this.docUrl('algorithms/consensus', this.props.docsPart)}>
+              Consensus
+            </a>
+          </div>
+          <div>
+            <h5>CLI</h5>
+            <a href={this.docUrl('cli/dfms/dfms', this.props.docsPart)}>
+              DFMS
+            </a>
+            <a href={this.docUrl('cli/dfmsr/dfmsr', this.props.docsPart)}>
+              DFMSR
+            </a>
+          </div>
+          <div>
+            <h5>Giudes</h5>
+            <a href={this.docUrl('giudes/contract/compose', this.props.docsPart)}>
+              Contract
+            </a>
+            <a href={this.docUrl('giudes/drive/add', this.props.docsPart)}>
+              Drive
+            </a>
+            <a href={this.docUrl('giudes/supercontract/deploy', this.props.docsPart)}>
+              SuperContract
+            </a>
+            <a href={this.docUrl('giudes/net/id', this.props.docsPart)}>
+              Network
             </a>
           </div>
         </section>
